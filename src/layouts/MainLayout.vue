ff0f68abcb5b17a3d7604050733b858ad0ddb407
bgtor <neurologiemd@gmail.com>
2024-06-15 14:41:42 +0300

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,95 @@
+<template>
+  <q-bar class="app-title-bar q-electron-drag">
+    <q-btn
+      dense
+      flat
+      round
+      icon="lens"
+      size="8.5px"
+      color="red"
+    />
+    <q-btn
+      dense
+      flat
+      round
+      icon="lens"
+      size="8.5px"
+      color="yellow"
+    />
+    <q-btn
+      dense
+      flat
+      round
+      icon="lens"
+      size="8.5px"
+      color="green"
+    />
+    <div class="col text-center text-weight-bold">My-App</div>
+  </q-bar>
+  <q-layout view="lHh Lpr lFf">
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+defineOptions({
+  name: 'MainLayout'
+})
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+const leftDrawerOpen = ref(false)
+
+function toggleLeftDrawer () {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="flex flex-center">
+    <div style="width: 50%">
+      <q-select label="Input" v-model="model" :options="options"></q-select>
+    </div>
+
+  </q-page>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+defineOptions({
+  name: 'IndexPage'
+});
+const model = ref(null);
+const options = [
+  'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle',
+  'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle',
+  'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle',
+  'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
+];
+</script>
